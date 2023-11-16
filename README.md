a2764ad49fc249179cde144e445b22096f8b38e1
Danrley Pereira <danrleywillian@gmail.com>
2023-11-16 01:43:16 -0300

    Environment variable from file .env

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 pip install -r requirements.txt
 
 ## set virtual env
-sudo apt-get install python3-venv
-python3 -m venv .
-source ./bin/activate
+- sudo apt-get install python3-venv
+- python3 -m venv .
+- source ./bin/activate
--- openai-test.py
+++ openai-test.py
@@ -1,5 +1,10 @@
 from openai import OpenAI
-client = OpenAI()
+from pdb import set_trace
+from dotenv import dotenv_values
+
+config = dotenv_values()
+
+client = OpenAI(api_key=config.get('OPENAI_API_KEY'))
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
@@ -10,3 +15,4 @@ completion = client.chat.completions.create(
 )
 
 print(completion.choices[0].message)
+set_trace()
